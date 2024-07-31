fd2fb1352f0d1a5455c212e407c06c5daf7c9b7c
kpbongat <kpbongat.dev@gmail.com>
2024-07-31 17:59:55 +0800

    Add append method to LinkedList

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 class LinkedList {
-  #list = [];
+  head;
+  tail;
+  append(value) {
+    if (!this.head) {
+      const newNode = new Node(value);
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.pointer != null) {
+        currentNode = currentNode.pointer;
+      }
+      const newNode = new Node(value);
+      currentNode.pointer = newNode;
+      this.tail = newNode;
+    }
+  }
 }
 
 class Node {
